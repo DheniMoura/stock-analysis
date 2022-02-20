949d196b118b63e45fed706bf641e95ee7ed3129
DheniMoura <dhenifer@hotmail.com>
2022-02-20 18:21:40 -0300

    Commit

--- stock_analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from stastmodels.tsa.seasonal import seasonal_decompose
-import yfinance as yf
-
-ticket = yf.Ticker('ABEV')
-# df = ticket.history(period='3y', insterval='1mo')
-df = ticket.history(interval='1d', start='2018-01-01', end='2022-01-01')
